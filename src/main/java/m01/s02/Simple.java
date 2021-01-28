9ca31744c10ec6ea33603f0238868e23a6bf1d80
simonica101 <sm_frtd@hotmail.it>
2021-01-28 12:00:06 +0100

    minor change

--- src/main/java/m01/s02/Simple.java
+++ src/main/java/m01/s02/Simple.java
@@ -8,7 +8,7 @@ import java.lang.Math; // not required
 /**
  * A "hello world" class
  * 
- * @author Emanuele Galli
+ * @author Emanuele Galli  //comment minor change
  */
 public class Simple {
     public static void main(String[] args) {
